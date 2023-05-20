77f23c19870479f887a25cff0b75f2c9b9881a6e
devrzaul <98711027+devrzaul@users.noreply.github.com>
2023-05-20 20:47:34 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+//Find the even and odd numbers from 1 and 1000 through for loop
+
+for (let i = 1; i <= 1000; i++) {
+    
+    if ( i % 2 == 0) {
+        console.log(`Loop Even No : ${i}`);
+        
+    } else {
+        console.log(`Loop odd No : ${i}`);
+    }
+    
+}
